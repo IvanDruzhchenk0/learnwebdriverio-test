b10015fa37efd32de95c09c72daf5f1fdb816964
IvanDruzhchenk0 <druzhchenko.ivan@gmail.com>
2024-06-01 11:53:38 +0300

    refactor: use const instead of selectors

--- tests/Authorization.spec.ts
+++ tests/Authorization.spec.ts
@@ -9,34 +9,40 @@ test.describe("Authorization", () => {
   const email = createEmail("gmail.com");
   const name = createName();
 
+  const emailField = 'input[placeholder="Email"]';
+  const passwordField = 'input[placeholder="Password"]';
+  const signInButton = '//button[contains(text(), "Sign in")]';
+  const editProfileButton = '//*[@data-qa-id="follow-toggle"]';
+  const profileUserName = '//*[@data-qa-id="profile-username"]';
+
   test("Sign Up", async ({ page }) => {
     await page.locator('//a[contains (@href,"/register")]').click();
     await page.locator('input[placeholder="Username"]').fill(name);
-    await page.locator('input[placeholder="Email"]').fill(email);
-    await page.locator('input[placeholder="Password"]').fill("Qwerty123@");
+    await page.locator(emailField).fill(email);
+    await page.locator(passwordField).fill("Qwerty123@");
     await page.locator('//button[contains(text(), "Sign up")]').click();
 
     await page.getByRole("link", { name: `${name}` }).click();
-    await expect(page.locator('//*[@data-qa-id="follow-toggle"]')).toBeVisible();
-    await expect(page.locator('//*[@data-qa-id="profile-username"]')).toBeVisible();
+    await expect(page.locator(editProfileButton)).toBeVisible();
+    await expect(page.locator(profileUserName)).toBeVisible();
   });
 
   test("Sigh In", async ({ page }) => {
     await page.locator('//a[contains (@href,"/login")]').click();
-    await page.locator('input[placeholder="Email"]').fill(email);
-    await page.locator('input[placeholder="Password"]').fill("Qwerty123@");
-    await page.locator('//button[contains(text(), "Sign in")]').click();
+    await page.locator(emailField).fill(email);
+    await page.locator(passwordField).fill("Qwerty123@");
+    await page.locator(signInButton).click();
 
     await page.getByRole("link", { name: `${name}` }).click();
-    await expect(page.locator('//*[@data-qa-id="follow-toggle"]')).toBeVisible();
-    await expect(page.locator('//*[@data-qa-id="profile-username"]')).toBeVisible();
+    await expect(page.locator(editProfileButton)).toBeVisible();
+    await expect(page.locator(profileUserName)).toBeVisible();
   });
 
    test("Logout", async ({ page }) => {
     await page.locator('//a[contains (@href,"/login")]').click();
-    await page.getByPlaceholder("Email").fill(email);
-    await page.getByPlaceholder("Password").fill("Qwerty123@");
-    await page.locator('//button[contains(text(), "Sign in")]').click();
+    await page.locator(emailField).fill(email);
+    await page.locator(passwordField).fill("Qwerty123@");
+    await page.locator(signInButton).click();
     await page.locator('//a[contains (@href,"/settings")]').click();
     await page.locator('//button[contains(text(), "click here to logout")]').click();
 
