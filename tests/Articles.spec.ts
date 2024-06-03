b0ebad6b60bbf019e8bd063ba377c114c87f2891
IvanDruzhchenk0 <druzhchenko.ivan@gmail.com>
2024-06-03 11:49:32 +0300

    refactor some locators and names, add test for deleting article from favorites

--- tests/Articles.spec.ts
+++ tests/Articles.spec.ts
@@ -5,7 +5,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-test.describe("Create Article", () => {
+test.describe("Articles", () => {
   const email = createEmail("gmail.com");
   const name = createName();
 
@@ -22,16 +22,16 @@ test.describe("Create Article", () => {
 
     await page.locator('//a[contains (@href,"/editor")]').click();
     await page
-      .locator('//*[@data-qa-id="editor-title"]')
+      .locator('[data-qa-id="editor-title"]')
       .fill("Test article Ivan");
     await page
-      .locator('//*[@data-qa-id="editor-description"]')
+      .locator('[data-qa-id="editor-description"]')
       .fill("This article is about creating article");
     await page
       .locator('//*[@class="form-group"]//textarea')
       .fill("I'd like to type some dummy text :)");
-    await page.locator('//*[@data-qa-id="editor-tags"]').fill("testing");
-    await page.locator('//button[@data-qa-id="editor-publish"]').click();
+    await page.locator('[data-qa-id="editor-tags"]').fill("testing");
+    await page.locator('button[data-qa-id="editor-publish"]').click();
 
     await expect(
       page.locator(
@@ -55,15 +55,51 @@ test.describe("Create Article", () => {
       .getByRole("link", { name: `${name}` })
       .first()
       .click();
-
     await page
-      .locator('//button[@data-qa-type="article-favorite"]')
+      .locator('button[data-qa-type="article-favorite"]')
       .first()
       .click();
     await page.locator('//a[contains (@href,"/favorites")]').click();
     await expect(
       page.locator(
         '//*[@data-qa-type="preview-title"][contains(text(), "Test article")]'
+      )
+    ).toBeVisible();
+    await page.locator('//li[@data-qa-type="feed-tab"]/a[contains(text(), "My Articles")]').click();
+    await expect(
+      page.locator(
+        '//*[@data-qa-type="article-favorite-count"][contains(text(), "1")]'
+      )
+    ).toBeVisible();
+  });
+
+  test("Delete article from favorite", async ({ page }) => {
+    await page.locator('//a[contains (@href,"/login")]').click();
+    await page.locator(emailField).fill(email);
+    await page.locator(passwordField).fill("Qwerty123@");
+    await page.locator(signInButton).click();
+    await page
+      .getByRole("link", { name: `${name}` })
+      .first()
+      .click();
+
+    await page.locator('//a[contains (@href,"/favorites")]').click();
+    await page.locator('button[data-qa-type="article-favorite"]').click();
+    await page.reload();
+    await expect(
+      page.locator(
+        '//*[@data-qa-type="preview-title"][contains(text(), "Test article")]'
+      )
+    ).toBeHidden();
+    await expect(
+      page.locator(
+        '//*[@data-qa-type="article-list"]//div[contains(text(), "No articles are here")]'
+      )
+    ).toBeVisible();
+    await page.locator('//li[@data-qa-type="feed-tab"]/a[contains(text(), "My Articles")]').click();
+    await expect(
+      page.locator(
+        '//*[@data-qa-type="article-favorite-count"][contains(text(), "0")]'
       )
     ).toBeVisible();
   });
